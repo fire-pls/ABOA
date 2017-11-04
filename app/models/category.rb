9557b1b01e76a8c4a5a0ea1d1cda60f820c3fe82
Trevor Malard <redsky.dz@gmail.com>
2017-11-04 01:44:16 -0700

    associations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,4 @@
 class Category < ApplicationRecord
+  has_many :stocks
+  has_many :items, through: :stocks
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_one :cart, dependent: :destroy
+  has_many :orders, dependent: :destroy
+  has_many :reservations, through: :cart
+  has_many :items, through: :orders
 end

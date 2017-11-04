5d6188acaccb0df0100e76d6e93e54529d2dfe21
Trevor Malard <redsky.dz@gmail.com>
2017-11-04 09:12:35 -0700

    seed syntax error fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,5 @@ if User.blank?
 end
 
 if Stock.blank?
-  Stock.create(name:'Black Tshirt',description:'Fitted crew neck. 100% cotton'category:Category.create(name:"MENS"))
+  Stock.create(name:'Black Tshirt',description:'Fitted crew neck. 100% cotton',category:Category.create(name:"MENS"))
 end
